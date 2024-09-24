32252823bfcd4bbbc4802661ae9cdc3598b8dea2
dolmatovDan <58562604+dolmatovDan@users.noreply.github.com>
2024-09-24 22:25:41 +0300

    changes ;)

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -1,8 +1,14 @@
 return {
-	"GustavEikaas/easy-dotnet.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-	config = function()
-		local dotnet = require("easy-dotnet")
-		dotnet.setup()
-	end,
+  "GustavEikaas/easy-dotnet.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+  config = function()
+    local dotnet = require("easy-dotnet")
+    dotnet.setup({})
+    vim.keymap.set("n", "<leader>nt", function()
+      dotnet.test_solution()
+    end)
+    vim.keymap.set("n", "<leader>nr", function()
+      dotnet.run_default()
+    end)
+  end,
 }
