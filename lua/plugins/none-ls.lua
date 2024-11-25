013f9330d3bb9a67557ecb030feaf993ddc31b2b
dolmatovDan <58562604+dolmatovDan@users.noreply.github.com>
2024-11-25 11:00:17 +0300

    add git flog

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,39 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.erb_lint,
-        -- null_ls.builtins.diagnostics.clang_format,
-        -- null_ls.builtins.diagnostics.csharpier,
-        -- null_ls.builtins.diagnostics.rubocop,
-        -- null_ls.builtins.formatting.rubocop,
-      },
-    })
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.erb_lint,
+				-- null_ls.builtins.diagnostics.clang_format,
+				-- null_ls.builtins.diagnostics.csharpier,
+				-- null_ls.builtins.diagnostics.rubocop,
+				-- null_ls.builtins.formatting.rubocop,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+							-- vim.lsp.buf.formatting_sync()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
